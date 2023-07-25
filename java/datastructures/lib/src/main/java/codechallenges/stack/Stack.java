d9db61692fc8c460b992fccc61a0b1a9cdcc5684
Wasim Soomro <103798599+WasimSoomro@users.noreply.github.com>
2023-07-25 17:10:17 -0400

    linked-list-kth

---
+++ java/datastructures/lib/src/main/java/codechallenges/stack/Stack.java
@@ -0,0 +1,29 @@
+package codechallenges.stack;
+import codechallenges.stack.Node;
+public class Stack<T>
+{
+  Node<T> top;
+
+  public void push(T valueToPush)
+  {
+    // TODO: implement me
+  }
+
+  public T pop()
+  {
+    // TODO: implement me
+    return null;
+  }
+
+  public T peek()
+  {
+    // TODO: implement me
+    return null;
+  }
+
+  public boolean isEmpty()
+  {
+    // TODO: implement me
+    return false;
+  }
+}
